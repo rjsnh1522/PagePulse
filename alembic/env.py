8d68aa6df1e2a207853c298e251e474b76914043
pawan <rjsnh1522@gmail.com>
2025-04-27 11:30:31 +0530

    updated alembic conf with port

--- alembic/env.py
+++ alembic/env.py
@@ -27,8 +27,9 @@ db_name = os.getenv("DB_NAME")
 db_user = os.getenv("DB_USER")
 db_pass = os.getenv("DB_PASS")
 db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
 
-DB_URL = f"postgresql+psycopg2://{db_user}:{db_pass}@{db_host}/{db_name}"
+DB_URL = f"postgresql+psycopg2://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
 
 
 # this is the Alembic Config object, which provides
